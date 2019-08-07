f87c5714eb895e1730f09377c073654397930bec
bill.huang <beahuang@htomail.com>
2019-08-07 14:42:04 +0800

    init

---
+++ src/main/java/com/netmap/jadeite/JadeiteApplication.java
@@ -0,0 +1,13 @@
+package com.netmap.jadeite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadeiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadeiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netmap/jadeite/JadeiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.netmap.jadeite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JadeiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
